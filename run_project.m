ade12ca461c588b3d74c185df3af06bf877736c3
mark <markv>
2017-05-03 17:53:51 +0200

    2

--- run_project.m
+++ run_project.m
@@ -7,5 +7,6 @@ model = l2('model');
 %model.simulate(5, 'COM3');
 model.simulate(50);
 %model.plot();
-model.plot({'anxiety','sitfac', 'hr', 'breathing_f', 'physical_state','chest_c'});
-% sitfac, hr, breathing_f, physical_state,chest_c
+                    %do not plot beliefs etc
+model.plot({'anxiety','sitfac', 'hr', 'breathing_f', ...
+  'physical_state','chest_c'});
