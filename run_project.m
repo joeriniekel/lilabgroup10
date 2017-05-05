4b0fcb19f3976a6381431fed5466cd6b8ff471a2
mark <markv>
2017-05-05 13:36:30 +0200

    Still error in scenario.l2

--- run_project.m
+++ run_project.m
@@ -4,10 +4,13 @@ clc
 disp('Running model...')
 model = l2('model');
 %model.simulate(5, 'COM3');
-model.simulate(50);
+model.simulate(50,'default','default');
 disp('Simulation finished');
 %model.plot();
-                    %do not plot beliefs etc
+
+model.plot({'test_predicate'})
+
+
 model.plot({'anxiety','sitfac', 'hr',...
     'breathing_f', 'physical_state',...
     'chest_c','graph_bel_chest_pos',...
