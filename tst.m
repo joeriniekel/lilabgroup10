897764db447c225c820a9d8e508449a696ac860b
user4224 <markvoschezang@gmail.com>
2017-05-09 14:25:08 +0200

    k

--- tst.m
+++ tst.m
@@ -8,7 +8,18 @@ phi = 0.2475*pi;
 f = 0.21;
 dt = 0.5;
 
-t = 0:0.01:3;
-plot(t,sin(t*3 + 0.0*pi));
+%plotting the breathing f
+t = 0:0.01:10;
+f = 1.3;
+plot(t,sin(t*f));grid on;
+%the value of x(9) = sin(9*f)
+t_9_f_9 = sin(9*f)
+%assume that on t=9 the there is a change in f
+f = 1.5;
+plot(t,sin(t*f));grid on;
+t_10_f_1_2 = sin(10*f)
+%now the value of x seems to have skipped several time steps
 
-sin(2*pi* f * dt + phi)
+
+
+%sin(2*pi* f * dt + phi)
