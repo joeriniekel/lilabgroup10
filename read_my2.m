c85a01825b901d420406b810b29601830ded836e
mark <markv>
2017-05-22 18:18:02 +0200

    D

--- read_my2.m
+++ read_my2.m
@@ -1,12 +1,12 @@
 clc
 
 % length(c1_bf)
-c1_bf = csvread('data/calculated/bb_c1_v56.csv');
-c1_hr = csvread('data/calculated/hr_c1_v56.csv');
-c2_bf = csvread('data/calculated/bb_c2_v44.csv');
-c2_hr = csvread('data/calculated/hr_c2_v44.csv');
-c3_bf = csvread('data/calculated/bb_c3_v69 long.csv');
-c3_hr = csvread('data/calculated/hr_c3_v69 long.csv');
+% c1_bf = csvread('data/calculated/bb_c1_v56.csv');
+% c1_hr = csvread('data/calculated/hr_c1_v56.csv');
+c2_bf = csvread('data/calculated/bb_c2_v86.csv');
+c2_hr = csvread('data/calculated/hr_c2_v86.csv');
+% c3_bf = csvread('data/calculated/bb_c3_v69 long.csv');
+% c3_hr = csvread('data/calculated/hr_c3_v69 long.csv');
 
 length(c1_bf)
 
